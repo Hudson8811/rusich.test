df362de7b3de0233fded6a4cd44ed6252c65b261
PipZip <37911683+PipZip@users.noreply.github.com>
2019-11-28 13:52:45 +0600

    temp

--- build/js/main.js
+++ build/js/main.js
@@ -46,7 +46,22 @@ tpl:'<div class="fancybox-share"><h1>{{SHARE}}</h1><p><a class="fancybox-share__
 /* my scripts */
 
 $(document).ready(function() {
-
-
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        dots: false,
+        asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+        slidesToShow: 6,
+        slidesToScroll: 6,
+        asNavFor: '.slider-for',
+        dots: false,
+        arrows: false,
+        focusOnSelect: true,
+        infinite: false
+    });
 
 });
